2a8de4374d54b295d5932b1326272df09079e32f
SC139625 <pyc298@nyu.edu>
2024-04-30 19:07:50 -0400

    trainer datafield bugs and misnaming

--- trainer.py
+++ trainer.py
@@ -8,42 +8,46 @@ import torch
 import torch.functional as F
 
 class PokerTrainer:
-    def __init__(self, num_players=4, buffer_capacity=10000, batch_size=32):
+    def __init__(self, num_players=4, buffer_capacity=10000, batch_size=32, lr = 0.01):
         #initialization for the game
         self.num_players = num_players
         self.game = PokerGame(num_players=num_players)
         self.buffer = ReplayBuffer(capacity=buffer_capacity)
+        self.lr = lr
 
         #train loop datafields instantiations
         self.batch_size = batch_size
         self.train_steps = 0
         self.eval_games = 0
-        self.global_step = 0
+        self.global_step = 10
         self.total_eval_rewards = 0 
 
         #Actor parameter instantiation
         self.stddev = 5 
         self.hidden_dim = 256 #experiement with this value
         self.action_shape = 1
-        self.actor = Actor( repr_dim= len(self.game_vectorized_state()), action_shape= self.action_shape, hidden_dim=self.hidden_dim)
-        self.adversary = Actor( repr_dim= len(self.game_vectorized_state()), action_shape= self.action_shape, hidden_dim=self.hidden_dim)
+        self.actor = Actor( repr_dim= len(self.game.get_vectorized_state()), action_shape= self.action_shape, hidden_dim=self.hidden_dim)
+        self.adversary = Actor( repr_dim= len(self.game.get_vectorized_state()), action_shape= self.action_shape, hidden_dim=self.hidden_dim)
         self.actor_opt = torch.optim.Adam(self.actor.parameters(), lr=self.lr)
 
         #Critic parameter instantion
-        self.critic = Critic(repr_dim = len(self.game_vectorized_states()),action_shape= self.action_shape, hidden_dim=self.hidden_dim)
+        self.critic = Critic(repr_dim = len(self.game.get_vectorized_state()),action_shape= self.action_shape, hidden_dim=self.hidden_dim)
         self.critic_opt = torch.optim.Adam(self.critic.parameters(), lr=self.lr)
-
+        
+        print("PokerTrainer Initialized\n")
         
     def choose_action(self, agent, state):
         #get an action from our actor
+        state = torch.Tensor(state)
         dist = agent(state, self.stddev)
         action = dist.sample()
-        return action.numpy() if isinstance(action, torch.Tensor) else action
+        return action.detach().numpy() if isinstance(action, torch.Tensor) else action
     
     def eval_action(self,agent,state):
+        state = torch.Tensor(state)
         dist = agent(state, self.stddev)
         action = dist.mean()
-        return action.numpy() if isinstance(action, torch.Tensor) else action
+        return action.detach().numpy() if isinstance(action, torch.Tensor) else action
     
     def eval(self):
         self.total_eval_rewards = 0
@@ -68,7 +72,7 @@ class PokerTrainer:
 
     def train(self):
         #total number of training steps 
-        for _ in self.global_step:
+        for _ in range(self.global_step):
             #start a game
             self.game = PokerGame(num_players=self.num_players)
             #get the starting state
@@ -76,7 +80,8 @@ class PokerTrainer:
             
             #for 10000 or what ever is the most recent game end...
             while self.train_steps < 10000:
-                action = self.choose_action(state, self.epsilon)
+                action = self.choose_action(self.actor, state)[0]
+                print(f'action: {action}')
                 done, batch = self.game.execute_action(action)
                 #if batch is not empty
                 #add to the buffer
@@ -136,8 +141,6 @@ class PokerTrainer:
 
         #update actor
         self.update_actor(states)
-
-      
 
     def log_progress(self):
         print(f"Step: {self.global_step}, Buffer Size: {len(self.buffer)}")
