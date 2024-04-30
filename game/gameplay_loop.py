6bb67d5ae212760b6f5ab6f0c31f480726ccfd1d
EfeKalyoncu <kalyoncuefe1997@gmail.com>
2024-04-30 18:16:19 -0400

    Make cards integer values in vectorizzation, add correct rewards

--- game/gameplay_loop.py
+++ game/gameplay_loop.py
@@ -1,5 +1,7 @@
 import game.game_state as gs
 import random
+import numpy as np
+import copy
 
 class PokerGame:
     def __init__(self, num_players=9):
@@ -7,6 +9,7 @@ class PokerGame:
         self.player_hands = []
         self.player_pot_commitment = []
         self.cards_reserved = []
+        self.player_capital_soh = []
         self.button_location = 0
         self.pot = 0
         self.action_position = 0
@@ -31,7 +34,12 @@ class PokerGame:
             self.player_capital.append(100)
             self.player_pot_commitment.append(0)
             self.player_hands.append(())
+        
+        self.init_hand()
     
+    def card_value_calculate(self, card):
+        return card.suite * 100 + card.number
+
     def find_next_action_location(self, starting_point):
         for i in range(self.num_players - 1):
             cur_player_no = (i + starting_point + 1) % self.num_players
@@ -44,17 +52,20 @@ class PokerGame:
         state = []
 
         for i in range(len(self.cards_on_board)):
-            state.append(self.cards_on_board[i])
+            state.append(self.card_value_calculate(self.cards_on_board[i]))
+        for i in range(5 - len(self.cards_on_board)):
+            state.append(-1)
         state.append(self.pot)
 
         state.append(self.button_location)
-        state.append(self.player_hands[self.action_position][0])
-        state.append(self.player_hands[self.action_position][1])
+        state.append(self.card_value_calculate(self.player_hands[self.action_position][0]))
+        state.append(self.card_value_calculate(self.player_hands[self.action_position][1]))
         state.append(self.action_position)
 
         for i in range(len(self.player_capital)):
             state.append(self.player_capital[i])
             state.append(self.player_pot_commitment[i])
+            state.append(len(self.player_hands[i]) / 2)
         
         return state
 
@@ -77,31 +88,34 @@ class PokerGame:
             else:
                 player_hand_seen.append(2)
                 hands_seen += 2
-            curhand = []
-            for j in range(len(self.cards_on_board)):
-                curhand.append(self.cards_on_board[j])
-            curhand.append(self.player_hands[i][0])
-            curhand.append(self.player_hands[i][1])
+                curhand = []
+                for j in range(len(self.cards_on_board)):
+                    curhand.append(self.cards_on_board[j])
+                curhand.append(self.player_hands[i][0])
+                curhand.append(self.player_hands[i][1])
 
-            if len(best_hand) == 0:
-                best_hand = curhand
-                best_player_location = [i]
-            else:
-                comparison_result = gs.compare_hands(curhand, best_hand)
-                if comparison_result == 0:
+                if len(best_hand) == 0:
                     best_hand = curhand
                     best_player_location = [i]
-                elif comparison_result == 1:
-                    continue
-                elif comparison_result == -1:
-                    best_player_location.append(i)
+                else:
+                    comparison_result = gs.compare_hands(curhand, best_hand)
+                    if comparison_result == 0:
+                        best_hand = curhand
+                        best_player_location = [i]
+                    elif comparison_result == 1:
+                        continue
+                    elif comparison_result == -1:
+                        best_player_location.append(i)
         
-        for i in range(len(self.state_action_reward_buffer)):
-            player_reward = self.state_action_reward_buffer[i][0][9]
-            # self.state_action_reward_buffer[i][2] = [, hands_seen - player_hand_seen[self.state_action_reward_buffer[i][0][9]]]
-
         for i in range(len(best_player_location)):
             self.player_capital[best_player_location[i]] += self.pot / len(best_player_location)
+        
+        for i in range(len(self.state_action_reward_buffer)):
+            # The player that should receive the reward
+            player_reward = self.state_action_reward_buffer[i][0][9]
+            self.state_action_reward_buffer[i][2] = [self.player_capital[player_reward] - self.state_action_reward_buffer[i][2][0], hands_seen - player_hand_seen[player_reward]]
+
+        
         
         self.pot = 0
         self.players_in_hand = 0
@@ -120,6 +134,7 @@ class PokerGame:
 
     def init_hand(self):
         self.button_location = (self.button_location + 1) % 9
+        self.player_capital_soh = copy.deepcopy(self.player_capital)
         for i in range(self.num_players):
             if self.player_capital[i] >= self.minimum_bet * 2:
                 self.player_hands[i] = (self.deck.pop(), self.deck.pop())
@@ -148,11 +163,12 @@ class PokerGame:
                     break
         
         if blinds_put_in < 2:
-            return -1
+            return -1, []
         self.action_position = action_player
         self.current_bet = self.minimum_bet * 2
+        returnBuffer = self.state_action_reward_buffer
 
-        return 0
+        return 0, returnBuffer
         
     def get_state_for_current_player(self):
         return_list = []
@@ -174,7 +190,7 @@ class PokerGame:
             return_list.append(self.player_pot_commitment[i])
     
     def execute_action(self, action):
-        state_action_reward = [self.get_vectorized_state, [action], [0, 0]]
+        state_action_reward = [self.get_vectorized_state(), [action], [self.player_capital_soh[self.action_position], 0]]
         bet_needed = self.current_bet - self.player_pot_commitment[self.action_position]
         min_raise = self.current_bet * 2 - self.player_pot_commitment[self.action_position]
         if action >= self.player_capital[self.action_position]:
@@ -203,6 +219,7 @@ class PokerGame:
         state_action_reward[1] = [action]
         self.player_pot_commitment[self.action_position] += action
         self.player_capital[self.action_position] -= action
+        print(state_action_reward)
         self.state_action_reward_buffer.append(state_action_reward)
         
         if self.players_in_hand == self.players_agreed_on_pot:
